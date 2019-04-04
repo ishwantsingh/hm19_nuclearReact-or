95792c305d9fd9bc2f8339878d7b715d952e20a5
Ishwant Singh <ishwant8@gmail.com>
2019-04-04 19:54:01 +0530

    reducer stores startindex, endIndex, distance, startStation, EndStation]

--- greenway/src/state/reducers.js
+++ greenway/src/state/reducers.js
@@ -3,14 +3,32 @@ import stations from "./dummyData";
 const initialState = {
   stations: stations,
   start: "",
-  end: ""
+  end: "",
+  startIndex: 0,
+  endIndex: 0,
+  distance: 0
 };
 
 export default function stationReducer(state = initialState, action) {
   switch (action.type) {
     case "GET_INFO":
-      console.log(action.payload);
-      return { ...state, start: action.payload.start, end: action.payload.end };
+      const startIndex = state.stations.findIndex(
+        item => item.name === action.payload.start
+      );
+      const endIndex = state.stations.findIndex(
+        item => item.name === action.payload.end
+      );
+      const distance =
+        state.stations[endIndex].position - state.stations[startIndex].position;
+      // console.log(state.stations);
+      return {
+        ...state,
+        start: action.payload.start,
+        end: action.payload.end,
+        distance: distance,
+        startIndex: startIndex,
+        endIndex: endIndex
+      };
     default:
       return state;
   }
